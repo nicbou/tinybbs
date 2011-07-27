6091364c38956dd923f0e3e90c5e00136828c142
Nicolas Bouliane <contact@nicolasbouliane.com>
2011-07-27 12:25:13 -0400

    Moved the javascript to the end

--- index.php
+++ index.php
@@ -4,6 +4,11 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>TinyBBS</title>
 	<link rel="stylesheet" type="text/css" href="css.css">
+</head>
+<body>
+	<div class="forum">
+	<?php require_once('tinybbs.php');?>
+	</div>
 	<script type="text/javascript">
 		document.forms[0].style.display="block";
 		function reply(parent){
@@ -11,10 +16,5 @@
 			forms[0].style.display="block";parent.style.display="none";
 		}
 	</script>
-</head>
-<body>
-	<div class="forum">
-	<?php require_once('tinybbs.php');?>
-	</div>
 </body>
 </html>
