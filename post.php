070c880858a3cfd835e53e7ebf5e28a0bfde63c6
Nicolas Bouliane <contact@nicolasbouliane.com>
2011-07-27 12:39:43 -0400

    Date returned as timestamp, shorter error messages, protected class variables

--- post.php
+++ post.php
@@ -1,10 +1,10 @@
 <?php
 require_once('db.php');
 class Post {
-	private $msg;
-	private $parent;
-	private $id;
-	private $date;
+	protected $msg;
+	protected $parent;
+	protected $id;
+	protected $date;
 	function __construct($msg,$parent,$id=NULL,$date=NULL){
 		$this->setMessage($msg);
 		$this->setParent($parent);
@@ -18,7 +18,7 @@ class Post {
 		if(is_numeric($parent) && !is_float($parent) && $parent >=0)
 			$this->parent=$parent;
 		else
-			throw new Exception("Can't assign parent to non-numeric value", '100');
+			throw new Exception("Parent has to non-numeric value", '100');
 	}
 	function setMessage($msg){
 		$this->msg=$msg;
@@ -27,7 +27,7 @@ class Post {
 		if(is_numeric($id) && !is_float($id) && $id >=0)
 			$this->id=$id;
 		else
-			throw new Exception("Can't assign id to non-numeric value", '100');
+			throw new Exception("ID has to non-numeric value", '100');
 	}
 	function setDate($date){
 		$this->date=$date;
@@ -35,5 +35,5 @@ class Post {
 	function getParent(){return $this->parent;}
 	function getMessage(){return $this->msg;}
 	function getId(){return $this->id;}
-	function getDate(){return $this->date;}
+	function getDate(){return strtotime($this->date);}
 }
